c2ebf85f64734b1f736a1cffe5a27c75c918e444
Greg Huber <gregoryh@uvic.ca>
2022-06-25 16:44:03 -0700

    update dependencies and change to new React

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,10 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+// import { RemixBrowser } from "@remix-run/react";
+// import { hydrate } from "react-dom";
 
-hydrate(<RemixBrowser />, document);
+// hydrate(<RemixBrowser />, document);
+
+import { RemixBrowser } from "@remix-run/react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document);
+root.render(<RemixBrowser />);
